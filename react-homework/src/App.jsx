9f75bb558cdad7884f6de34f4f0eb668d592f195
Leeseayoung <tnfusrltk12@naver.com>
2024-05-27 20:09:59 +0900

    feat: 숙제 완료!

--- react-homework/src/App.jsx
+++ react-homework/src/App.jsx
@@ -1,35 +1,47 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import React, { useState } from "react";
+import Fragment from "./Fragment";
 
-function App() {
+import "./Count.css";
+
+const App = () => {
   const [count, setCount] = useState(0);
 
+  //더하기
+  const addCount = () => {
+    setCount(count + 1);
+  };
+  //빼기
+  const minusCount = () => {
+    setCount(count - 1);
+  };
+
+  const resetCount = () => {
+    setCount(0);
+  };
+
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <Fragment />
+      {count}
+      <br />
+      <button className="btn1" onClick={addCount}>
+        +
+      </button>
+      <button className="btn3" onClick={resetCount}>
+        reset
+      </button>
+      <button className="btn2" onClick={minusCount}>
+        -
+      </button>
+    </div>
   );
-}
+};
 
 export default App;
+
+// 1. 숫자(초기값: 0)와 +, -, reset 버튼을 만든다. 완료!
+// 2. + 버튼 클릭 시 숫자를 1 증가시킨다. 완료!
+// 3. - 버튼 클릭 시 숫자를 1 감소시킨다. 완료!
+// 4.  reset 버튼 클릭 시 숫자를 다시 0으로 변경한다. 완료!
+// 5. className과 css 파일을 생성하고, 원하는 css 를 추가한다. (화려 X)
+// 6. 태그 최상단에 Fragment를 추가해본다.
---
+++ react-homework/src/Fragment.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Fragment = () => {
+  return <h3>카운트</h3>;
+};
+
+export default Fragment;
