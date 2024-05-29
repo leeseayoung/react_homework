790768f580fa4e1693b6bb9b7a045d964801966e
Leeseayoung <tnfusrltk12@naver.com>
2024-05-29 21:42:13 +0900

    중간 완료!

--- react-homework/src/App.jsx
+++ react-homework/src/App.jsx
@@ -107,3 +107,122 @@
 // }
 
 // export default App;
+
+//------------------------------------------------------------------------------------------- 2주차 과제
+
+// import React from "react";
+// import { useState } from "react";
+
+// const App = () => {
+//   // 이메일
+//   const [email, setEmail] = useState("");
+//   const emailChange = (e) => {
+//     setEmail(e.target.value);
+//   };
+//   // 비밀번호
+//   const [password, setPassword] = useState("");
+//   const passwordChange = (e) => {
+//     setPassword(e.target.value);
+//   };
+
+//   // 비밀번호 확인
+//   const [passWordCheck, setPassWordCheck] = useState("");
+//   const passWordChecked = (e) => {
+//     setPassWordCheck(e.target.value);
+//   };
+
+//   //제출하기
+//   const handleSubmit = (event) => {
+//     //초기화 X
+//     event.preventDefault();
+
+//     if (!email) {
+//       alert(`이메일 채우셈`);
+//       return;
+//     }
+//     if (!password) {
+//       alert(`비번 채우셈`);
+//       return;
+//     }
+
+//     if (!passWordCheck) {
+//       alert(`비번확인 채우셈`);
+//       return;
+//     }
+
+//     if (password !== passWordCheck) {
+//       alert(`비번이 틀림!`);
+//       return;
+//     }
+//   };
+//   return (
+//     <form onSubmit={handleSubmit}>
+//       <div>
+//         <label>이메일 : </label>
+//         <input type="email" value={email} onChange={emailChange} />
+//       </div>
+
+//       <div>
+//         <label>비밀번호 : </label>
+//         <input type="password" value={password} onChange={passwordChange} />
+//       </div>
+//       <div>
+//         <label>비밀번호 확인 : </label>
+//         <input
+//           type="password"
+//           value={passWordCheck}
+//           onChange={passWordChecked}
+//         />
+//       </div>
+//       <button>회원가입</button>
+//     </form>
+//   );
+// };
+
+// export default App;
+
+import { useState } from "react";
+
+function App() {
+  const [text, setText] = useState("");
+  const textChange = (e) => {
+    setText(e.target.value);
+  };
+
+  const textBtn = (e) => {
+    e.preventDefault();
+    if (!text) {
+      alert("채우셈");
+      return;
+    }
+  };
+
+  return (
+    <div>
+      <h1>할 일 목록</h1>
+      <form onSubmit={textBtn}>
+        <input
+          type="text"
+          placeholder="할 일을 추가하세요"
+          value={text}
+          onChange={textChange}
+        />
+        <button type="submit">추가</button>
+      </form>
+      <ul>
+        <li style={{ textDecoration: "line-through" }}>
+          잠자기
+          <button>취소</button>
+          <button>삭제</button>
+        </li>
+        <li>
+          일찍 일어나기
+          <button>완료</button>
+          <button>삭제</button>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
