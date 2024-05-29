ec5b2292a293557b732c9bec7903b26d4adcbaf2
Leeseayoung <tnfusrltk12@naver.com>
2024-05-29 23:11:42 +0900

    feat: 미완료!

--- react-homework/src/App.jsx
+++ react-homework/src/App.jsx
@@ -180,15 +180,16 @@
 // };
 
 // export default App;
-
+// 아직 미완료!
 import { useState } from "react";
 
 function App() {
+  //인풋
   const [text, setText] = useState("");
   const textChange = (e) => {
     setText(e.target.value);
   };
-
+  //유효성
   const textBtn = (e) => {
     e.preventDefault();
     if (!text) {
@@ -197,6 +198,28 @@ function App() {
     }
   };
 
+  //추가하기
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: "잠자기",
+      completed: true,
+    },
+    {
+      id: 2,
+      text: "일찍 일어나기",
+      completed: false,
+    },
+  ]);
+
+  const newTodo = {
+    id: Date.now(),
+    text: text,
+    completed: false,
+  };
+
+  setTodos([...todos, newTodo]);
+  setText("");
   return (
     <div>
       <h1>할 일 목록</h1>
@@ -210,16 +233,16 @@ function App() {
         <button type="submit">추가</button>
       </form>
       <ul>
-        <li style={{ textDecoration: "line-through" }}>
-          잠자기
-          <button>취소</button>
-          <button>삭제</button>
-        </li>
-        <li>
-          일찍 일어나기
-          <button>완료</button>
-          <button>삭제</button>
-        </li>
+        {todos.map((todo) => (
+          <li
+            key={todo.id}
+            style={{ textDecoration: todo.completed ? "line-through" : "none" }}
+          >
+            {todo.text}
+            <button>{todo.completed ? "취소" : "완료"}</button>
+            <button>삭제</button>
+          </li>
+        ))}
       </ul>
     </div>
   );
